45d67c6d435501ae5465b0ede7cb81c4b9879eaa
Mark Claxton <mark.claxton@herd.io>
2026-01-26 22:04:48 +0000

    github pages deploy added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  experimental: {
-  },
-  vite: {
-    ssr: {
-      noExternal: ['swiper'],
-    }
-  }
-});
+  site: 'https://markclaxton.github.io',
+  base: '/site',
+})
