e311fc68ef5196eda5a2fa25d308bee37e1b130e
Mark Claxton <mark.claxton@herd.io>
2026-09-19 20:54:52 +0100

    site updates and fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  site: 'https://markclaxton.github.io',
-  base: '/site',
+  site: 'https://markclaxton.co.uk',
+  integrations: [sitemap()],
 })
